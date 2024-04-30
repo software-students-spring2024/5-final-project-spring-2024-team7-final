f8f8bccd2d0bf5b4d06a31d13371a213b0f88027
nhaulsey <ndhaulsey@gmail.com>
2024-04-30 17:56:00 -0400

    Added cliet fixture to make the testing tests work.

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,14 @@ from app import *
 from app import app
 
 @pytest.fixture
+def client():
+    
+    app.config.update({
+        
+        "TESTING" : True,
+    })
+    with app.test_client() as client:
+        yield client
 
 def test_sanity_check(client):
 
@@ -17,3 +25,4 @@ def test_sanity_check2(client):
     expected = True
     actual = True
     assert actual == expected, "Expected True to be equal to True!"
+    
