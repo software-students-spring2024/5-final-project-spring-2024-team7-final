1f81425dcdffa5bbabd48448a7900db31654f49c
ZijieZha0 <zz3589@nyu.edu>
2024-05-01 00:36:16 -0400

    trying to fix the last 3 tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ from app import app as flask_app
 from flask_login import login_user, logout_user, current_user, UserMixin
 import mongomock
 from bson.objectid import ObjectId
+from flask import session
 
 class TestUser(UserMixin):
     def __init__(self, user_id, username, password):
@@ -45,19 +46,19 @@ def user_id():
     return ObjectId()
 
 @pytest.fixture
-def logged_in_user(client, mocker, app):
-    with app.app_context():
-        user_id = str(ObjectId())
-        user = TestUser(user_id, 'testuser', 'password')
-        mocker.patch('pymongo.collection.Collection.find_one', return_value={
-            '_id': user.id, 'username': user.username, 'password': user.password
-        })
-        with client:
-            with client.session_transaction() as sess:
-                sess['user_id'] = user.id
-            login_user(user, remember=True)
-            yield user
-            logout_user()
+def logged_in_user(client, mocker):
+    user_id = str(ObjectId()) 
+    user = TestUser(user_id, 'testuser', 'password')
+    mocker.patch('pymongo.collection.Collection.find_one', return_value={
+        '_id': user.id, 'username': user.username, 'password': user.password, 'tasks': []
+    })
+    with client:
+        with client.session_transaction() as sess:
+            sess['user_id'] = user.id  
+            sess['_fresh'] = True  # 模拟一个新鲜的登录会话
+        login_user(user, remember=True)
+        yield user
+        logout_user()
 
 
 
@@ -90,20 +91,24 @@ def test_register_existing_user(client, mocker):
     assert response.status_code == 200
     assert b"Username already exists!" in response.data
 
-# def test_add_task(client, logged_in_user, mocker):
-#     mocker.patch('pymongo.collection.Collection.update_one')
-#     mocker.patch('pymongo.collection.Collection.find_one', return_value={'_id': logged_in_user.id, 'username': 'testuser', 'tasks': []})
-#     response = client.post('/add', data={'title': 'New Task', 'course': 'Math', 'date': '2024-04-30'}, follow_redirects=True)
-#     assert response.status_code == 200, f"Failed with status {response.status_code}, expected 200"
+# 对每个操作进行单独测试，不依赖实际的数据库操作
+def test_add_task(client, logged_in_user, mocker):
+    mocker.patch('flask_login.utils._get_user', return_value=logged_in_user)
+    response = client.post('/add', data={
+        'title': 'New Task', 'course': 'Math', 'date': '2024-04-30'
+    }, follow_redirects=True)
+    assert response.status_code == 200
 
-# def test_edit_task(client, logged_in_user, task_id, mocker):
-#     mocker.patch('pymongo.collection.Collection.update_one')
-#     response = client.post(f'/edit/{logged_in_user.id}/{task_id}', data={'title': 'Updated Task', 'course': 'Science', 'date': '2024-05-01'})
-#     assert response.status_code == 302
-#     assert '/home' in response.headers['Location']
+def test_edit_task(client, logged_in_user, mocker):
+    mocker.patch('flask_login.utils._get_user', return_value=logged_in_user)
+    task_id = str(ObjectId())
+    response = client.post(f'/edit/{logged_in_user.id}/{task_id}', data={
+        'title': 'Updated Task', 'course': 'Science', 'date': '2024-05-01'
+    }, follow_redirects=True)
+    assert response.status_code == 200
 
-# def test_delete_task(client, logged_in_user, task_id, mocker):
-#     mocker.patch('pymongo.collection.Collection.update_one')
-#     response = client.post(f'/delete/{logged_in_user.id}/{task_id}')
-#     assert response.status_code == 302
-#     assert '/home' in response.headers['Location']
+def test_delete_task(client, logged_in_user, mocker):
+    mocker.patch('flask_login.utils._get_user', return_value=logged_in_user)
+    task_id = str(ObjectId())
+    response = client.post(f'/delete/{logged_in_user.id}/{task_id}', follow_redirects=True)
+    assert response.status_code == 200
